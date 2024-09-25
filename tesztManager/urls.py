be4029e8e4631949fd59d921e7ef92275c72e059
Bpat <40142657+GPFlopi@users.noreply.github.com>
2024-09-25 18:56:25 +0200

    rename

--- tesztManager/urls.py
+++ tesztManager/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for passManager project.
+URL configuration for tesztManager project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
